5c33e224f1c9007f3f1b2a8ec2c0df6a7a2c0975
VasilyevVA <vladislavovix@live.com>
2022-05-16 12:56:29 +0700

    1) зайти на сайт appleinsider.ru 2) нажать на кнопку поиска 3) ввести текст "Чем iPhone 13 отличается от iPhone 12" и нажать ENTER 4) в найденных статьях получить href атрибут первой найденной статьи 5) убедиться, что href ссылка содержит слово iPhone 13 (обратить внимание как это слово записано в href)

---
+++ src/test/java/AppleTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppleTest extends BaseTest {
+
+    private final static String BASE_URL = "https://appleinsider.ru/";
+    private final static String SEARCH_STRING = "Чем iPhone 13 отличается от iPhone 12";
+    private final static String EXPECTED_WORD = "iphone-13";
+
+    @Test
+    public void checkHref() {
+
+        MainPage mainPage = new MainPage(BASE_URL);
+        mainPage.search(SEARCH_STRING);
+        SearchPage searchPage = new SearchPage();
+        String href = searchPage.getHrefFromFirstArticle();
+        Assert.assertTrue(href.contains("iphone-13"));
+
+        //Вариант записи в одну строку
+        Assert.assertTrue(new MainPage(BASE_URL)
+                .search(SEARCH_STRING)
+                .getHrefFromFirstArticle()
+                .contains(EXPECTED_WORD));
+
+        //mainPage.openWebSite(BASE_URL);
+
+    }
+
+}
